5bfa19ad0d5f6ccd079fb6fcf8d4bf2b3d1a0ad5
Gabriel Henrique <gcoelho730@gmail,com>
2023-12-02 23:29:07 -0300

    react code

--- src/App.js
+++ src/App.js
@@ -1,58 +1,113 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import api from "./services/api";
 import "./styles.css";
 
 function App() {
-	const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitles] = useState('');
+  const [url, setUrls] = useState('');
+  const [techs, setTechs] = useState([]);
 
-	useEffect(() => {
-		api.get("/repositories").then((response) =>
-			setRepositories(response.data)
-		);
-	}, []);
 
-	async function handleAddRepository() {
-		// TODO
-		const response = await api.post("/repositories", {
-			title: "Prog Web",
-			url: "ufopa.edu.br",
-			techs: ["Python", "Node.JS", "ReactJS"],
-		});
+  useEffect(() => {
+    api.get("/repositories").then((response) =>
+      setRepositories(response.data)
+    );
+  }, []);
 
-		setRepositories([...repositories, response.data]);
-	}
+  async function handleAddRepository() {
+    const response = await api.post("/repositories", { title, url, techs });
 
-	async function handleRemoveRepository(id) {
-		// TODO
-		await api.delete(`repositories/${id}`);
+    console.log(response.data);
 
-		setRepositories(
-			repositories.filter((repository) => repository.id !== id)
-		);
-	}
+    setRepositories([...repositories, response.data]);
+    
+    // Clear input fields after submission
+    setTitles('');
+    setUrls('');
+    setTechs([]);
+  }
 
-	return (
-		<div>
-			<ul data-testid="repository-list">
-				{repositories.map((repository) => (
-					<li key={repository.id}>
-						{repository.title}
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
 
-						<button
-							onClick={() =>
-								handleRemoveRepository(repository.id)
-							}
-						>
-							Remover
-						</button>
-					</li>
-				))}
-			</ul>
+    setRepositories(
+      repositories.filter((repository) => repository.id !== id)
+    );
+  }
 
-			<button onClick={handleAddRepository}>Adicionar</button>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await handleAddRepository();
+  };
+
+  async function handleLikeRepository(id) {
+	// Implement "Like Repository" functionality
+	const likeRepository = await api.post(`repositories/${id}/like`);
+
+	const newRepository = repositories.map((repository) => {
+		if (repository.id === id) {
+			return likeRepository.data;
+		} else {
+			return repository;
+		}
+	});
+
+	setRepositories(newRepository);
+}
+
+  return (
+    <>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title} {repository.likes}
+            <button onClick={() => handleRemoveRepository(repository.id)} className="btn-remove">
+              Remover
+            </button>	
+			<button onClick={() => handleLikeRepository(repository.id)} className="btn-like ">
+              Like
+            </button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+		<div className="add-repository-container">
+      <div className="add-items">
+			<input
+			type="text"
+			name="title"
+			placeholder="title"
+			required
+			value={title}
+			onChange={(e) => setTitles(e.target.value)}
+			/>
+      </div>
+      <div className="add-items">
+			<input
+			type="text"
+			name="url"
+			placeholder="url"
+			required
+			value={url}
+			onChange={(e) => setUrls(e.target.value)}
+			/>
+      </div>
+      <div className="add-items">
+			<textarea
+			name="techs"
+			placeholder="Techs"
+			required
+			value={techs}
+			onChange={(e) => setTechs(e.target.value)}
+			/>
+      </div>
 		</div>
-	);
+        <button type="submit" className="btn-add">Adicionar</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
